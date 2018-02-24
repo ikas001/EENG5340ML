6bfafbad7ebb53453db31a43f959572c9ed712c0
ikas001 <ialbasheir@scorpion.splitbregman.com>
2018-02-24 01:15:43 -0600

    Revert "solving conflicts"
    This reverts commit 0f3c58713f59b15423966d39686d7a7e91dc4cc2, reversing
    changes made to 6c3c5c073f9ae77df476fb3a24309b3a131c7194.
    
    rolling back

--- project1.sh
+++ project1.sh
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 for ((i=30;i<100;i+=10));do
 #	echo $i
     python project1_Main.py $i
-=======
-fname="results.txt"
-if [ -f fname ] then
-	rm $fname
-
-for ((i=30;i<70;i+=10));do
-#	echo $i
-    python project1.py $i $fname
->>>>>>> Main/master
 done
